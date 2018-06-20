ef59f0110804e0868b0c44333b9c04aa11f91e86
leavez <gaojiji@gmail.com>
2018-06-20 17:36:27 +0800

    clean project

--- Mappable_xcode_plugin/AppDelegate.swift
+++ Mappable_xcode_plugin/AppDelegate.swift
@@ -13,20 +13,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
-        
-//        let path = Bundle.main.url(forResource: "ModelSample", withExtension: nil)!.path
-//        let content = try! String.init(contentsOfFile: path)
 
-        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
 
 }
 
